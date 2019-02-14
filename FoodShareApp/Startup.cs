f534b2a6da992dce8e86430f3c9c20d6b021b6e3
unknown <link8801@hotmail.com>
2019-02-14 13:02:42 -0500

    initial commit with gitignore

---
+++ FoodShareApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FoodShareApp.Startup))]
+namespace FoodShareApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
