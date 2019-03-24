2942e2244b9346fa5788cc06f51d7ef27646a8c7
Joel Murphy <200203095@student.georgianc.on.ca>
2019-03-24 19:45:24 -0400

    Added a viewmodel for the dashboard finally, got structure of Dashboard

---
+++ FoodShareApp/Controllers/DashboardController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodShareApp.Controllers
+{
+    [Authorize]
+    public class DashboardController : Controller
+    {
+        private DbFoodShare db = new DbFoodShare();
+
+        // GET: TestDash
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            Models.DashboardViewModel myModel = new Models.DashboardViewModel
+            {
+                foodProvider = db.FoodProviders.ToList(),
+                food = db.Foods.ToList().Where(f => f.FoodProviderId == userId)
+            };
+            return View(myModel);
+        }
+    }
+}
