539bfa0c66cf38f9d7080f5348942acfe4ad2dce
Joel Murphy <200203095@student.georgianc.on.ca>
2019-03-24 01:08:48 -0400

    Trying to do the Dashboard model/controller. Fixed Register radio buttons

---
+++ FoodShareApp/Controllers/DashboardController.cs
@@ -0,0 +1,39 @@
+﻿using FoodShareApp.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodShareApp.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private AdminDashboardModel adminModel = new AdminDashboardModel();
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            //var adminDashboardModels = new AdminDashboardModel();
+            ////adminDashboardModels.AllFoodProviders = List<db.FoodProviders>;
+            //var foodProviders = db.FoodProviders;
+            //var userId = User.Identity.GetUserId();
+            //var foods = db.Foods.Where(f => f.FoodProviderId == userId);
+
+            //var dashModel = new AdminDashboardModel { AllFoodProviders = foodProviders.ToList(), AllFoods = foods.ToList() };
+            //return View(dashModel);
+            //var foodProviders = db.FoodProviders;
+            //var userId = User.Identity.GetUserId();
+            //var foods = db.Foods.Where(f => f.FoodProviderId == userId);
+            //var dashModel = new 
+            //var dashModel = new AdminDashboardModel { foodProviders.ToList(), AllFoods = foods.ToList() };
+            //adminModel.AllFoodProviders = GetFoodProviders();
+            var dashModel = adminModel;
+            return View(adminModel);
+        }
+        //public ActionResult Details(string id)
+        //{
+        //    var dashModel = new AdminDashboardModel
+        //}
+    }
+}
---
+++ FoodShareApp/Models/AdminDashboardModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FoodShareApp.Models
+{
+    public class AdminDashboardModel
+    {
+        public IEnumerable<FoodProvider> AllFoodProviders { get; set; }
+        public IEnumerable<Food> AllFoods { get; set; }
+        public IEnumerable<Event> AllEvents { get; set; }
+    }
+}
