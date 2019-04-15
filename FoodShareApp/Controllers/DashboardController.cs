1a0b5d4887a58be723068543bb414cf0711dc6d7
omarshah5885 <200323449@student.georgianc.on.ca>
2019-04-14 21:18:05 -0400

    added make admin func to dashboard

--- FoodShareApp/Controllers/DashboardController.cs
+++ FoodShareApp/Controllers/DashboardController.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using Microsoft.AspNet.Identity.EntityFramework;
+using FoodShareApp.Models;
 
 namespace FoodShareApp.Controllers
 {
@@ -24,7 +27,53 @@ namespace FoodShareApp.Controllers
                 food = db.Foods.ToList().Where(f => f.FoodProviderId == userId),
                 events = db.Events.ToList()
             };
+
+
+
             return View(myModel);
+        }
+
+        // Post Dashboard 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(string UserId, ApplicationDbContext application)
+        {
+            //_applicationDb = application;
+            if (ModelState.IsValid)
+            {
+                var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(application));
+                var user = userManager.FindById(UserId);
+                // var user = _applicationDb.Users.Find(UserId);
+
+                var roleId = user.Roles.SingleOrDefault().RoleId;
+                var roleName = application.Roles.SingleOrDefault(r => r.Id == roleId).Name;
+
+                userManager.RemoveFromRole(UserId, roleName);
+                userManager.AddToRole(UserId, "Admin");
+
+                FoodProvider Provider = db.FoodProviders.FirstOrDefault(f => f.FoodProviderId == UserId);
+                Provider.Admin = true;
+                db.SaveChanges();
+
+                application.Entry(user).State = System.Data.Entity.EntityState.Modified;
+
+            var userId = User.Identity.GetUserId();
+            ViewBag.userId = userId.ToString();
+            Models.DashboardViewModel myModel = new Models.DashboardViewModel
+            {
+                foodProvider = db.FoodProviders.ToList(),
+                food = db.Foods.ToList().Where(f => f.FoodProviderId == userId),
+                events = db.Events.ToList()
+            };
+                // return RedirectToAction("../Home");
+                return View(myModel);
+            }
+
+
+
+
+            return View();
+
         }
 
         public ActionResult Notifications(string User)
